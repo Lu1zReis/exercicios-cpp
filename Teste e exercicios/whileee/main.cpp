b5000e7f1bb745f269122b179a0ec866da28beed
Luiz <eluiz8204@gmail.com>
2021-02-23 10:50:20 -0400

    primeiro commit de c

---
+++ Teste e exercicios/whileee/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=0;
+    while(a<100){
+    cout << a << endl;
+    int b;
+    cin >> b;
+    a=a+b;
+    }
+    return 0;
+}
