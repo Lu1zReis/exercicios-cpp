51bc18a335137c5ba654a227462bd48492e19b82
Luiz <eluiz8204@gmail.com>
2023-02-12 21:34:44 -0400

    despesas beta

--- Teste e exercicios/POO/despesas/main.cpp
+++ Teste e exercicios/POO/despesas/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <sstream>
+
 using namespace std;
 fstream arquivo;
 
 struct conta {
     string nome;
     string dia, mes;
+    string data;
     string valor;
 };
 
@@ -34,6 +40,138 @@ string getValor(string *l) {
     return valor;
 }
 
+string getData(string *l) {
+    string linha = *l;
+    string valor;
+    int cont = 0;
+    for(int i = 0; i < linha.size(); i++) {
+        if(linha[i] == ';') {
+            for(int j = i+1; j < linha.size(); j++) {
+                if(linha[j] == ';') {
+                    return valor;
+                }
+                valor += linha[j];
+            }
+        }
+
+    }
+}
+
+string formataData(string *d) {
+    string novaD = *d;
+    if(novaD.size() == 1) {
+        novaD.insert(0, "0");
+    }
+    return novaD;
+}
+
+int maiorNome() {
+
+    string linha, nome;
+    vector<string> nomes;
+
+    int pos = 0, maior;
+
+    arquivo.open("bancoDados.txt", ios::in);
+    if(arquivo.is_open()) {
+        while(getline(arquivo, linha)) {
+            int cont = 0;
+            for(int i = 0; i < linha.size(); i++) {
+                if(linha[i] == ';') {
+                    cont++;
+                }
+                if(cont == 0) {
+                    nome += linha[i];
+                }
+            }
+            nomes.push_back(nome);
+            nome = "";
+        }
+        arquivo.close();
+    }
+
+    for(int j = 0; j < nomes.size(); j++) {
+        if(j == 0) {
+            maior = nomes[j].size();
+        } else {
+            if(maior < nomes[j].size()) {
+                maior = nomes[j].size();
+            }
+        }
+    }
+
+    return maior;
+}
+
+int maiorvalor() {
+    string linha, valor;
+    vector<string> valores;
+
+    int pos = 0, maior;
+
+    arquivo.open("bancoDados.txt", ios::in);
+    if(arquivo.is_open()) {
+        while(getline(arquivo, linha)) {
+            int cont = 0;
+            for(int i = 0; i < linha.size(); i++) {
+                if(linha[i] == ';') {
+                    cont++;
+                }
+                if(cont == 2) {
+                    valor += linha[i];
+                }
+            }
+            valores.push_back(valor);
+            valor = "";
+        }
+        arquivo.close();
+    }
+
+    for(int j = 0; j < valores.size(); j++) {
+        if(j == 0) {
+            maior = valores[j].size();
+        } else {
+            if(maior < valores[j].size()) {
+                maior = valores[j].size();
+            }
+        }
+    }
+
+    return maior;
+}
+
+int ultmPosicao() {
+    char contLinha[] = {};
+    string linha, valor;
+    int cont=0, x=0, valorFinal;
+
+    arquivo.open("bancoDados.txt", ios::in);
+
+    if(arquivo.is_open()) {
+        while(getline(arquivo, linha)) {
+            for(int i = 0; i < linha.size(); i++) {
+                if(linha[i] == ';')
+                    break;
+                if(cont == 0) {
+                    contLinha[i] = linha[i];
+                    x++;
+                    //cout << contLinha[i] << " "<< x <<endl;
+                }
+            }
+        }
+        arquivo.close();
+    }
+
+    // verificando se é um número
+    for(int j = 0; j < x; j++) {
+        if(isdigit(contLinha[j])) {
+            valor += contLinha[j];
+        }
+    }
+    istringstream(valor) >> valorFinal;
+    return valorFinal;
+}
+
 void addConta() {
     // VARIAVEIS E OBJETOS DA FUNCAO
     conta adicionar;
@@ -48,6 +186,10 @@ void addConta() {
         cin >> adicionar.dia >> adicionar.mes;
         cout << "Digite o valor da conta: ";
         cin >> adicionar.valor;
+
+        adicionar.dia = formataData(&adicionar.dia);
+        adicionar.mes = formataData(&adicionar.mes);
+
         arquivo << adicionar.nome << ";" << adicionar.dia << "/" <<adicionar.mes << ";" << adicionar.valor << endl;
         cout << "Deseja continuar?[s/n] ";
         cin >> opc;
@@ -58,19 +200,36 @@ void addConta() {
 
 void listContas() {
     // VARIAVEIS E OBJETOS DA FUNCAO
-
+    int nomeSize = maiorNome();
+    int contaSize = maiorvalor();
+    // int valorSize = maiorValor();
+    cout << nomeSize << endl;
+    cout << contaSize << endl;
     string linha;
     conta lista;
     arquivo.open("bancoDados.txt", ios::in);
 
     if(arquivo.is_open()) {
-        cout << "   NOME         VALOR    DATA\n";
+        cout << "   NOME           R$ VALOR       DATA\n";
         while(getline(arquivo, linha)) {
              // PEGANDO OS RESPECTIVOS VALORES DAS LINHAS
             lista.nome = getNome(&linha);
-             lista.valor = getValor(&linha);
+            lista.valor = getValor(&linha);
+            lista.data = getData(&linha);
 
-             cout << "  " << lista.nome << "        " << lista.valor << endl;
+            //CENTRALIZANDO O TEXTO (NOME PARA VALOR)
+            cout << "  " << lista.nome << "         ";
+            int diferenca = nomeSize - lista.nome.size();
+            for(int i = 0; i < diferenca; i++)
+                cout << " ";
+
+            // CENTRALIZANDO O TEXTO (VALOR PARA DATA)
+            int diferenca2 = contaSize - lista.valor.size();
+            for(int i = 0; i < diferenca2; i++)
+                cout << " ";
+            cout << lista.valor << "         " << lista.data << endl;
+
+
         }
         arquivo.close();
     } else {
@@ -127,6 +286,6 @@ int main()
         cout << "\n---------------------\n";
         sair = menu();
     }
-
+    cout << ultmPosicao() << endl;
     return 0;
 }
---
+++ Teste e exercicios/tela/tela.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "okay" << endl;
+	return 0;
+}
